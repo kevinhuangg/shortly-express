9d52fc42f187e1b9274b43836f3424d89551301a
kevinhuangg <email.kevinh@gmail.com>
2017-03-07 21:51:09 -0800

    implements part of session/cookie storing

--- server/middleware/sessionParser.js
+++ server/middleware/sessionParser.js
@@ -5,27 +5,20 @@ var _ = require('underscore');
 var createSession = function(req, res, next) {
   //check if cookies exist
   var agent = req.get('User-Agent');
-  console.log(agent);
 
-  if (!req.cookies.shortlyid) {
-    Sessions.addSession(agent).then(function(hash) {
+  if (!req.cookies.shortlyid) { //if theres no current session
+    return Sessions.addSession(agent).then(function(hash) {
       req.session = {hash: hash};
-      res.cookies['shortlyid'] = hash;
+      res.cookie('shortlyid', {hash: hash});
       next();
     }); 
-  }
-    //if no
-      //create new session
-      //set new cookie on response
-    //if yes
-      //check if sessionidhash exists in database
-      //if not
-        //destroy cookie
-        //create new session
-        //set new cookie response
-      //if exists
-        //get session from database
-        //assign session object to request
+  } 
+  //if there's a current session
+  Sessions.getSession(req.cookies.shortlyid).then(function(sessionID) {
+    console.log(sessionID);
+  });
+    //get session from db (input: req.cookies.shortlyid)
+
 
 
   
--- server/models/session.js
+++ server/models/session.js
@@ -5,9 +5,23 @@ var util = require('../lib/utility');
 
 module.exports = {
   addSession: function(userAgent) {
+    userAgent = userAgent || Math.floor((Math.random() * 100) + 1).toString(); //comment out later
     var hash = util.generateHash(userAgent);
     sessionObj = {hash: hash};
 
-    return db.queryAsync('insert into session set ?', sessionObj).return(hash);
+    return db.queryAsync('insert into sessions set ?', sessionObj).return(hash);
+  },
+  
+  getSession: function(cookie) {
+    var queryString = 'select * from sessions where hash = ?';
+
+    return db.queryAsync(queryString, cookie).then(function(results) {
+      var session = results[0][0];
+      console.log(session);
+      if (!session) {
+        return session;
+      }
+      
+    });
   }
 };
